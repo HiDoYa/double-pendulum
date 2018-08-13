7f417a477f4533025324a6da01ca52817d7d4aed
HiDoYa <hiroyagojo@gmail.com>
2018-08-12 19:13:29 -0700

    Math is hard

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ node_border = 2
 
 # Initialize
 pygame.init()
-size = (1280, 720)
+width = 1280
+height = 720
+size = (width, height)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Double Pendulum")
 done = False
@@ -25,6 +27,7 @@ started = False
 drag = False
 drag_target = 0
 init_mouse_pos = [0, 0]
+init_displacement = [0, 0]
 
 dist_nodes = 200
 
@@ -50,15 +53,16 @@ def mouse_in_circ(node_pos):
         return True
 
 def check_drag_target():
-    global started
     global drag
     global init_mouse_pos
     global drag_target
     global node_1_pos
     global node_2_pos
 
-    if not started and drag:
+    if not started:
         init_mouse_pos = pygame.mouse.get_pos()
+        init_displacement[0] = node_1_pos[0] - node_0_pos[0]
+        init_displacement[1] = node_1_pos[1] - node_0_pos[1]
 
         if mouse_in_circ(node_1_pos):
             drag_target = 1
@@ -69,8 +73,8 @@ def check_drag_target():
             drag = False
 
 def before_start():
-    global new_mouse_pos
     global node_1_pos
+    global node_2_pos
     global dist_nodes
 
     new_mouse_pos = pygame.mouse.get_pos()
@@ -78,28 +82,45 @@ def before_start():
     y_dist = new_mouse_pos[1] - init_mouse_pos[1]
 
     # wtf is this math
-    if drag and drag_target == 1:
-        i = node_1_pos[0] + x_dist
-        j = node_1_pos[1] + y_dist + node_0_pos[1]
+    if drag:
+        if drag_target == 1:
+            i = node_1_pos[0] + x_dist + init_displacement[0]
+            j = node_1_pos[1] + y_dist + init_displacement[1]
+            A = node_0_pos
+            B = [i, j]
 
-        A = node_0_pos
-        B = [i, j]
+            magnitude = (math.sqrt(math.pow((B[0] - A[0]), 2.0) + math.pow((B[1] - A[1]), 2.0)))
+            old_node_1_pos = node_1_pos[:]
 
-        magnitude = (math.sqrt(math.pow((B[0] - A[0]), 2.0) + math.pow((B[1] - A[1]), 2.0)))
+            node_1_pos[0] = int(A[0] + dist_nodes * (B[0] - A[0]) / magnitude)
+            node_1_pos[1] = int(A[1] + dist_nodes * (B[1] - A[1]) / magnitude)
 
-        node_1_pos[0] = int(A[0] + dist_nodes * (B[0] - A[0]) / magnitude)
-        node_1_pos[1] = int(A[1] + dist_nodes * (B[1] - A[1]) / magnitude)
-    elif drag_target == 2:
-        i = node_2_pos[0] + x_dist
-        j = node_2_pos[1] + y_dist + node_0_pos[1]
+            node_2_pos[0] += (node_1_pos[0] - old_node_1_pos[0])
+            node_2_pos[1] += (node_1_pos[1] - old_node_1_pos[1])
 
-        A = node_1_pos
-        B = [i, j]
+        elif drag_target == 2:
+            i = node_2_pos[0] + x_dist + init_displacement[0]
+            j = node_2_pos[1] + y_dist + init_displacement[1]
+            A = node_1_pos
+            B = [i, j]
 
-        magnitude = (math.sqrt(math.pow((B[0] - A[0]), 2.0) + math.pow((B[1] - A[1]), 2.0)))
+            magnitude = (math.sqrt(math.pow((B[0] - A[0]), 2.0) + math.pow((B[1] - A[1]), 2.0)))
 
-        node_2_pos[0] = int(A[0] + dist_nodes * (B[0] - A[0]) / magnitude)
-        node_2_pos[1] = int(A[1] + dist_nodes * (B[1] - A[1]) / magnitude)
+            node_2_pos[0] = int(A[0] + dist_nodes * (B[0] - A[0]) / magnitude)
+            node_2_pos[1] = int(A[1] + dist_nodes * (B[1] - A[1]) / magnitude)
+
+def run_sim():
+    cm_1 = []
+    cm_2 = []
+
+    height_1 = (node_1_pos[1] - node_0_pos[1]) - (node_0_pos[1] - dist_nodes)
+    PE_1 = mass_1 * grav * (node_1_pos[1] - node_0_pos[1])
+    # let height be the height from its lowest point possible
+
+    # L = KE(rotational) + KE(linear) - PE
+
+def coords_chg(coord, obj_heigh):
+    return (coord[0], height - coord[1] - obj_height)
 
 while not done:
     for event in pygame.event.get():
@@ -111,10 +132,18 @@ while not done:
         elif event.type == pygame.MOUSEBUTTONUP:
             drag = False
 
+    # Setting up
     before_start()
+
     screen.fill(GRAY)
 
+    # Start simulation
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_SPACE]:
+        started = true
 
+
+    # Draw
     pygame.draw.line(screen, BLACK, node_0_pos, node_1_pos)
     pygame.draw.line(screen, BLACK, node_1_pos, node_2_pos)
 
@@ -127,8 +156,10 @@ while not done:
     pygame.draw.circle(screen, BLACK, node_1_pos, radius, node_border)
     pygame.draw.circle(screen, BLACK, node_2_pos, radius, node_border)
 
-    # TODO
-    pygame.draw.circle(screen, BLACK, node_0_pos, dist_nodes, node_border)
+    if drag and drag_target == 1:
+        pygame.draw.circle(screen, BLACK, node_0_pos, dist_nodes, node_border)
+    elif drag and drag_target == 2:
+        pygame.draw.circle(screen, BLACK, node_1_pos, dist_nodes, node_border)
 
     # Update
     pygame.display.flip()
